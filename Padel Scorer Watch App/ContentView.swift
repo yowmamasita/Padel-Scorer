9c4481edcd46685c648d1ba38c81d942c2a9d295
Ben Sarmiento <ben.sarmiento@forto.com>
2023-05-18 17:50:37 +0200

    Initial Commit

---
+++ Padel Scorer Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Padel Scorer Watch App
+//
+//  Created by Ben Sarmiento on 18.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Padel Scorer Watch App/Padel_ScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Padel_ScorerApp.swift
+//  Padel Scorer Watch App
+//
+//  Created by Ben Sarmiento on 18.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Padel_Scorer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
